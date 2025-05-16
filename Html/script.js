09942d7dba25c17ded86c4bce4d741a966b392f9
sandhiya <sandhiya190905@gmail>
2025-05-16 16:00:25 +0530

    second commit

---
+++ Html/script.js
@@ -0,0 +1,27 @@
+var a=20;
+var b=10;
+console.log(a)
+console.log(b)
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a%b)
+console.log(a&b)
+console.log(a<=b)
+console.log(a>=b)
+console.log(a<b)
+console.log(a>b)
+function add(){
+    var a=10;
+    var b=30;
+    console.log(a=+b)
+}
+add()
+function add(a, b){
+    console.log(a+b)
+        
+    }
+    add(20,30)
+    add(50,30)
+    add(10,30)
